9fec71d4be8699dd3562afe1ffa1f1fc821b0193
hasmik_s2 <hassarg76@mail.ru>
2020-09-05 10:27:52 +0400

    Factorial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+unsigned int factorial(unsigned int n)
+{
+  if (n == 0 or n == 1) {
+    return 1;
+  }
+  return n * factorial(n-1);
+}
+
 int main()
 {
+  unsigned int number;
+  std::cin >> number;
+  std::cout << factorial(number) << std::endl;
   return 0;
 }
